8e072337d39ab84304378bf707735d541dca2b37
Junho Yeo <hanaro0704@gmail.com>
2020-10-10 23:27:37 +0900

    fix(api): temporary code to find out api request path

--- api/index.py
+++ api/index.py
@@ -7,7 +7,8 @@ app = Sanic(name='hermes-daangn')
 
 
 @app.route('/')
-@app.route('/<page:int>')
-async def index(request, page: int = 1):
-    products = get_products(page)
-    return json(products, ensure_ascii=False)
+@app.route('/<path:path>')
+async def index(request, path):
+    # products = get_products(page)
+    # return json(products, ensure_ascii=False)
+    return json({'hello': path})
